c99ecea26ee72034db33b33a0e76eb4a09a10ef7
capJavert <dev@kickass.website>
2025-12-01 21:38:02 +0100

    feat(day-1): part 2

--- src/bin/day_1.rs
+++ src/bin/day_1.rs
@@ -1,27 +1,52 @@
+use std::collections::HashMap;
+
 use advent_of_code_2025::fetch_input;
 
 fn main() -> Result<(), reqwest::Error> {
     let input = fetch_input(1, 2025).expect("failed to fetch input");
 
-    let mut value = 50;
+    let mut value: i64 = 50;
+    let mut combination: i64 = 0;
 
-    let rotate = |current, num| -> i32 { ((current + num) % 100 + 100) % 100 };
+    let mut cache = HashMap::new();
 
-    let mut combination = 0;
+    let mut rotate = |num: i64| -> () {
+        let cache_key = (value, num);
+
+        if let Some(cached_value) = cache.get(&cache_key) {
+            value = ((value + num) % 100 + 100) % 100;
+
+            combination += cached_value;
+
+            return;
+        }
+
+        let mut combination_iter = 0;
+
+        for _ in 0..num.abs() {
+            let sign = if num > 0 { 1 } else { -1 };
+
+            value = ((value + sign) % 100 + 100) % 100;
+
+            if value == 0 {
+                combination_iter += 1;
+            }
+        }
+
+        cache.insert(cache_key, combination_iter);
+
+        combination += combination_iter;
+    };
 
     for line in input.lines() {
         let (turn, num) = line.split_at(1);
-        let num: i32 = num.parse().expect("failed to parse number");
+        let num: i64 = num.parse().expect("failed to parse number");
 
-        value = match turn {
-            "R" => rotate(value, num),
-            "L" => rotate(value, -num),
+        match turn {
+            "R" => rotate(num),
+            "L" => rotate(-num),
             _ => panic!("invalid turn"),
         };
-
-        if value == 0 {
-            combination += 1;
-        }
     }
 
     println!("{}", combination);
